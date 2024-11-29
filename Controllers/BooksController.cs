812f01cbf997813be23e18eb72e79c8ccbce5ab6
Edwina Sweeney <Edwina.Sweeney@atu.ie>
2024-11-29 15:53:01 +0000

    Added code to Index

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -14,10 +14,15 @@ namespace LibraryMVCGroup.Controllers
         }
         public IActionResult Index()
         {
-            var availableBooks =  _dbContext.Books.Where(b => b.IsAvailable).ToList();
+            var availableBooks = _dbContext.Books.Where(b => b.IsAvailable == true).Include(b => b.Library).GroupBy(b => b.Library).Select(g => new
+            {
+                Library = g.Key, // The Library entity
+                Books = g.ToList() // The list of books in this library
+            });
+    
             return View(availableBooks);
         }
 
-        
+        //left off here - going to group by Library and sjhow all books available.
     }
 }
